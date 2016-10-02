ddf3312128073de4a8323ce6966499ac90dc9d36
Nikolay Nikolaev <ramos_95@mail.ru>
2016-10-02 09:41:32 +0300

    создание GUI зоопарка(2)

---
+++ js/Zoo.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Myself on 01.10.2016.
+ */
+function eating() {
+    var animals = new Array();
+
+}
+function Go() {
+
+    var a = document.getElementById("numEatZayaz_0").firstChild;
+    var b = a.nodeValue;
+    var c = 4;
+    b = c;
+
+
+}
